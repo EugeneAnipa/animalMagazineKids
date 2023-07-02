8393cc1b48233bd856afb2b105a992a6fc6e1deb
EugeneAnipa <e.anipa@student.unisi.it>
2023-07-01 23:45:10 -0500

    animal magazine for kids with sounds
    animal magazine using html css js

---
+++ dom.js
@@ -0,0 +1,45 @@
+function playSoundDog() {
+  var sound = new Audio("sounds/dog.wav");
+  $(".dogh3").text("I am Dog");
+  sound.play();
+}
+
+function playSoundCat() {
+  var sound = new Audio("sounds/cat.wav");
+  sound.play();
+}
+
+function playSoundCow() {
+  var sound = new Audio("sounds/cow.mp3");
+  sound.play();
+}
+
+function playSoundHorse() {
+  var sound = new Audio("sounds/horse.mp3");
+  sound.play();
+}
+
+function playSoundElephant() {
+  var sound = new Audio("sounds/elephant.mp3");
+  sound.play();
+}
+
+function playSoundMonkey() {
+  var sound = new Audio("sounds/monkey.mp3");
+  sound.play();
+}
+
+function playSoundLion() {
+  var sound = new Audio("sounds/lion.mp3");
+  sound.play();
+}
+
+function playSoundBear() {
+  var sound = new Audio("sounds/bear.mp3");
+  sound.play();
+}
+
+function playSoundPenguin() {
+  var sound = new Audio("sounds/penguin.mp3");
+  sound.play();
+}
